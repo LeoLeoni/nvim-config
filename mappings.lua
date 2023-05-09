3532de92f2f5acbfec350521875e1cf0f65afb40
jfleone <justin.leone@visionistinc.com>
2023-05-09 16:21:02 -0400

    More lsp mappings

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,7 @@ M.disabled = {
     ["<leader>fw"] = "", -- replaced with fg
     ["<leader>n"] = "", -- nvchad defaults for set number and relativenumber. Never changing.
     ["<leader>rn"] = "",
+    ["<leader>gt"] = "", -- replaced with gs
   },
 }
 
@@ -20,7 +21,10 @@ M.general = {
 M.telescope = {
   plugin = true,
   n = {
-    ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "live grep" }
+    ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "live grep" },
+    ["<leader>gr"] = { "<cmd> Telescope lsp_references <CR>", "telescope references" },
+    ["<leader>fs"] = { "<cmd> Telescope lsp_workspace_symbols <CR>", "workspace symbols" },
+    ["<leader>gs"] = { "<cmd> Telescope git_status <CR>", "git status" },
   }
 }
 
