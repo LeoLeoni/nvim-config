856fec63ab7a65fd5b26d6e412e21c5427e49de3
jfleone <justin.leone@visionistinc.com>
2023-05-25 09:28:34 -0400

    Fixed telescope lsp keybindings

--- mappings.lua
+++ mappings.lua
@@ -3,7 +3,6 @@ local M = {}
 -- Disables nvchad default keymappings
 M.disabled = {
   n = {
-    ["<leader>fw"] = "", -- replaced with fg
     ["<leader>n"] = "", -- nvchad defaults for set number and relativenumber. Never changing.
     ["<leader>rn"] = "",
     ["<leader>gt"] = "", -- replaced with gs
@@ -23,7 +22,8 @@ M.telescope = {
   n = {
     ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "live grep" },
     ["<leader>gr"] = { "<cmd> Telescope lsp_references <CR>", "telescope references" },
-    ["<leader>fs"] = { "<cmd> Telescope lsp_workspace_symbols <CR>", "workspace symbols" },
+    ["<leader>fw"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "workspace symbols" },
+    ["<leader>fs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "workspace symbols" },
     ["<leader>gs"] = { "<cmd> Telescope git_status <CR>", "git status" },
   }
 }
